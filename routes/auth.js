e8e34d43de6352dd65596b1492d18379b5300fe3
necco-neko <nekonecconeko@gmail.com>
2024-12-09 18:33:24 +0900

    ユーザー登録成功/ログイン失敗からのページ遷移をスムーズに行えるよう改善

--- routes/auth.js
+++ routes/auth.js
@@ -12,13 +12,24 @@ router.post('/register', async (req, res) => {
     return res.status(400).send('Both username and password are required');
   }
 
+  // ユーザーがすでに存在する場合
   if (users[username]) {
-    return res.status(400).send('Username already exists');
+    return res.status(400).send(`
+        <div>
+            <p>Username already exists</p>
+            <a href="/">Go to login page</a>
+        </div>
+    `);
   }
 
   users[username] = { password };
   console.log('Registerd user', users);
-  res.send('User registered')
+  res.send(`
+    <div>
+        <p>User registered successfully !</p>
+        <a href="/">Go to login page</a>
+    </div>
+  `);
 });
 
 // ログインエンドポイント
@@ -27,12 +38,22 @@ router.post('/login', async (req, res) => {
 
   // 情報が存在しない
   if (!username && !password) {
-    return res.status(400).send('Invalid credentials');
+    return res.status(400).send(`
+        <div>
+            <p>Username or password does not exist</p>
+            <a href="/">Try again</a>
+        </div>
+    `);
   }
 
   // 入力が登録情報と異なる
   if (!users[username] || users[username].password !== password) {
-    return res.status(400).send('Invalid credentials');
+    return res.status(400).send(`
+        <div>
+            <p>Invalid credentials</p>
+            <a href="/">Try again</a>
+        </div>
+    `);
   }
 
   // ログイン成功
